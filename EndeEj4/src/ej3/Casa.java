448b2bbfe14c47820f8225f1888fc26ee7691ccd
kevin <kevinfdezdelanda@gmail.com>
2020-06-02 08:56:04 +0200

    Clase casa creada con 3 atributos y un metodo

---
+++ EndeEj4/src/ej3/Casa.java
@@ -0,0 +1,17 @@
+package ej3;
+
+public class Casa {
+    private float superficie;
+    private String calle;
+    private int codigoPostal;
+    
+    /**
+     * Un equipo de limpiadores deja la casa libre de gérmenes
+     * y desinfecta todas las superficies
+     * */    
+    public void limpiarLaCasa() {
+        String mensajito ="Te hemos limpiado la casa, está desinfectada";
+        System.out.println(mensajito);
+    }    
+}
+
